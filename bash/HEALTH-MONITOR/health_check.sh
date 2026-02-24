b33b9f597cff9c1a9b21f0cc728a7a7cbb535641
tolea01 <anatoliecurbat@gmail.com>
2026-02-24 14:00:53 -0500

    add memory usage option and disk usage option

--- bash/HEALTH-MONITOR/health_check.sh
+++ bash/HEALTH-MONITOR/health_check.sh
@@ -15,10 +15,30 @@ check_requirements() {
 check_system_load() {
  CPU_LOAD=$(uptime | awk -F 'load average:' '{print $2}' | cut -d, -f1 | xargs)
 
-	echo -e "CPU Load (1 min): $CPU_LOAD"
+	echo -ne "CPU Load (1 min): $CPU_LOAD"
 
 	if (( $(echo "$CPU_LOAD > 2.0" | bc -l) )); then
 		echo -e "[${RED}HIGHT LOAD${NC}]"
+	else
+		echo -e " [${GREEN}OK${NC}]"
+	fi
+
+	MEM_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+
+	printf "Memory Usage: %.2f%% " $MEM_USAGE
+
+	if (( $(echo "$MEM_USAGE > 80.0" | bc -l) )); then
+		echo -e "[${RED}CRITICAL${NC}]"
+	else
+		echo -e "[${GREEN}OK${NC}]"
+	fi
+
+	DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed ' s/%//g')
+
+	echo -ne "Disk Usage (/): ${DISK_USAGE}% "
+
+	if [[ "$DISK_USAGE" -gt 85 ]]; then
+		echo -e "[${RED}LOW SPACE${NC}]"
 	else
 		echo -e "[${GREEN}OK${NC}]"
 	fi
@@ -28,3 +48,5 @@ echo -e "${YELLOW}=== System Health Report - $(date) ===${NC}"
 
 check_requirements
 check_system_load
+
+echo -e "${YELLOW}--------------------------------------${NC}"
