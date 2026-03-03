95ea3eed8cb190539f659263a5aa8f4eea977659
Anatolie <anatoliecurbat@gmail.com>
2026-03-03 10:19:29 -0500

    *

--- bash/WEB-LOG-ANALYZER/sentinel.sh
+++ bash/WEB-LOG-ANALYZER/sentinel.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+LOG_FILE="./access.log"
+AWK_SCRIPT="./log-parser.awk"
+IP_API=""
+
+if [ ! -f "$LOG_FILE" ]; then
+	echo "Fisierul $LOG_FILE nu exista"
+	exit 1
+fi
+
+if [ -f "$AWK_SCRIPT" ]; then
+	chmod +x "$AWK_SCRIPT"
+else
+	echo "Scriptul $AWK_SCRIPT nu exista"
+	exit 1
+fi
+
+get_ip_info() {}
