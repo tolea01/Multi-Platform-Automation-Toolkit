77273c39b95bc59d420cdd690a59c60a1873742c
tolea01 <anatoliecurbat@gmail.com>
2026-03-05 09:48:34 -0500

    add functions: send_alert_message & log_parser_handler

--- bash/WEB-LOG-ANALYZER/sentinel.sh
+++ bash/WEB-LOG-ANALYZER/sentinel.sh
@@ -25,7 +25,7 @@ get_ip_info() {
  local ip_addr="$1"
 
  if [[ ! -z "$ip_addr" ]] && [[ ! "$ip_addr" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  echo "Eroare fromat adresa ip" >&2
+  echo "Error ip address format" >&2
   return 1
  fi
 
@@ -38,3 +38,31 @@ get_ip_info() {
 
  echo "$api_res" | jq -r '.country_name'
 }
+
+send_alert_message() {
+	local message="$1"
+	local webhook_url="https://discord.com/api/webhooks/1479104889316573216/G79JQqLAdp8HzKsc-TTGknHDkGBRvkqELxCHh06blXR-uC2DRVUF7p-NSgZ5j4LvFWt_"
+
+	curl -H "Content-Type: application/json" \
+		-X POST \
+		-d "{\"content\": \"$message\"}" \
+		"$webhook_url"
+}
+
+log_parser_handler() {
+	while read -r line; do
+		local type_attack=$(echo "$line" | awk -F ':' '{print $1}')
+		local ip=$(echo "$line" | awk -F ':' '{split($2, ip, " "); print ip[1]}')
+		local count=$(echo "$line" | awk -F'[()]' '{print $2}')
+		local country_ip=$(get_ip_info "$ip")
+
+		local msg="$type_attack | IP: $ip | Country: $country_ip | Attempts: $count"
+
+		printf 'sudo iptables -A INPUT -s "%s" -j DROP\n' "$ip" >> block_ip.sh
+
+		send_alert_message "$msg"
+
+	done < <("$AWK_SCRIPT" "$LOG_FILE")
+}
+
+log_parser_handler
